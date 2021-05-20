6452f73982c8f3f7d749acb33dfd414faf168deb
aparna-wht <72865533+aparna-wht@users.noreply.github.com>
2021-05-20 15:52:23 +0530

    Add files via upload

---
+++ myproject-57/myproject-57/Screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet,TouchableOpacity } from 'react-native';
+
+export default class HomeScreen extends React.Component{
+  render()
+  {
+    return(
+      
+      <TouchableOpacity>
+        <Text>Read a Joke</Text>
+        </TouchableOpacity>
+     
+    )
+  }
+}
